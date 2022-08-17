7957fe7dcf95ed930a8c905aba3968b702601851
Jacob Sarosiek <jsarosiek22@gmail.com>
2022-08-17 00:10:20 -0500

    employee searching

--- employees.py
+++ employees.py
@@ -10,20 +10,23 @@ cnx = mysql.connector.connect(user='root', password='MySQL22!',
 @app.route('/')
 @app.route('/employees')
 def GetEmployees():
-	employees = ''
+	lastname = request.args.get('lastname')
+	employees = '<table><tr><th>First Name</th></tr>'
 	cursor = cnx.cursor(buffered=True)
+	querystr = 'SELECT * FROM employees'
 
-	query = ("SELECT * FROM employees")
+	if lastname != '':
+		querystr = querystr + ' WHERE last_name like "%' + lastname + '%"'
+
+	query = (querystr)
 	cursor.execute(query)
-	myresult = cursor.fetchmany(10)
-
-	print(myresult)
-	employees = 'test'
-
-	cursor.close()
-	cnx.close()
+	myresult = cursor.fetchmany(10) 
+	cursor.close() 
+ 
+	employees = myresult
+	
 	return render_template('employees.html', employees = employees)
-
+ 
 if __name__ == '__main__':
 	app.debug = True
-	app.run(host = '0.0.0.0', port = 5000)
+	app.run(host = '0.0.0.0', port = 5000) 
